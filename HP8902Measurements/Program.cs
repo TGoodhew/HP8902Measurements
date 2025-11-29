890856ec9d6289debd73d612d795d594b9bf677b
Tony Goodhew <Tony@schnauzergroup.com>
2025-11-29 07:01:23 -0800

    Add project files.

---
+++ HP8902Measurements/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HP8902Measurements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
